ef28b4f2a0a45d78575934ab7b201220550b16dc
DustinGrau-PSC <dugrau@progress.com>
2025-09-02 15:19:09 -0400

    Remove support for PAS monitoring under 11.7

--- PAS/Monitoring/Application/deploy/oe117/bin/host_setenv.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Get a non-loopback address to use for identification of this server.
-PUBLIC_IP=`ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p' | head -n 1`
-export PUBLIC_IP
